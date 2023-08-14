fb105ec3b0280c297c91970f029efac6ee937e71
Olatunde16 <olatundeolutunmibi@gmail.com>
2023-08-14 05:33:03 -0700

    Done

---
+++ 0x02-python-import_modules/hidden_4.py
@@ -0,0 +1,23 @@
+# uncompyle6 version 3.9.0
+# Python bytecode version base 3.8.0 (3413)
+# Decompiled from: Python 3.8.10 (default, Mar 15 2022, 12:22:08) 
+# [GCC 9.4.0]
+# Embedded file name: hidden_4.py
+# Compiled at: 2021-07-16 10:16:33
+# Size of source mod 2**32: 146 bytes
+
+
+def __init__():
+    return True
+
+
+def my_secret_santa():
+    return True
+
+
+def print_school():
+    return True
+
+
+def print_hidden():
+    return True
