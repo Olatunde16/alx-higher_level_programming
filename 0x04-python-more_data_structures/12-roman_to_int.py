683f0bdee2dcdbffb638315d8996baeff21bcf4e
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2023-08-17 14:42:18 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -14,6 +14,9 @@ def roman_to_int(roman_string):
     if not roman_string:
         return 0
 
+    if not isinstance(roman_string, str):
+        return 0
+
     rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     list_keys = list(rom_n.keys())
 
