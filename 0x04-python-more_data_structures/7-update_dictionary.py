e1442a6920b900366e81f1113c2ccbb57119c87a
Olutunmibi Iseoluwa Olatunde <olatundeolutunmibi@gmail.com>
2023-08-17 03:19:20 -0700

    Task 7 Done

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """A function that replaces or adds key or 
+    value in a dictionary."""
+    a_dictionary[key] = value
+    return a_dictionary
