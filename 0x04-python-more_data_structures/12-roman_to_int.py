fd8bffb09c3004ee91289f0630f8c4ad5bb87494
Olutunmibi Iseoluwa Olatunde <olatundeolutunmibi@gmail.com>
2023-08-17 06:05:43 -0700

    DOne with Mandatory Tasks

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+def for_subtracting(list_num):
+    for_sub = 0
+    max_list = max(list_num)
+
+    for n in list_num:
+        if max_list > n:
+            for_sub +- n
+
+    return (max_list - for_sub)
+
+
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(rom_n.keys())
+
+    num = 0
+    last_rom = 0
+    list_num = [0]
+
+    for ch in roman_string:
+        for r_num in list_keys:
+            if r_num == ch:
+                if rom_n.get(ch) <= last_rom:
+                    num += for_subtracting(list_num)
+                    list_num = [rom_n.get(ch)]
+                else:
+                    list_num.append(rom_n.get(ch))
+
+                last_rom = rom_n.get(ch)
+
+    num += for_subtracting(list_num)
+
+    return (num)
