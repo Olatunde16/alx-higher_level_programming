e3d12f490d6ebcf6f07fd68f93b12eb771414490
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2023-11-28 16:48:58 +0100

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """Print the alphabet in lowercase, not followed by a new line."""
 
-for char in range(ord('a'), ord('z') + 1):
-    print(chr(char), end='')
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
