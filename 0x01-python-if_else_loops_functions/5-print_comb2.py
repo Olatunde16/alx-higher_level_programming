fcbf39bae5757a2b511cc532c56b8a44398a3966
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2023-11-28 17:16:37 +0100

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number == 99:
+for num in range(100):
+    if num == 99:
         print("{}".format(number))
     else:
         print("{:02}".format(number), end=", ")
