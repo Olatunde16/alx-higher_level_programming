7aebaeef5457f67942b6bf438a1752cbbb21806b
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2023-11-28 16:59:51 +0100

    Update 4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -2,5 +2,5 @@
 # Author - Olutunmibi Iseoluwa
 
 """Print numbers 0 to 98 in decimal and hexadecimal."""
-for number in range(0, 99):
+for num in range(0, 99):
     print(f"{num} = 0x{num:02X}", end='\n' if num == 98 else '')
