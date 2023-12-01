041b4c894fee19f04c7875b46c4e2bdf2a82c4b6
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2023-12-01 18:39:20 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
-import hidden_4
-module_names = dir(hidden_4)
-filtered_names = [name for name in module_names if not name.startswith("__")]
-for name in sorted(filtered_names):
-    print(name)
+if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
