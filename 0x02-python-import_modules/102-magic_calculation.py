0788bbdfe89ddccf4b83ef2a3d27870ad5dec356
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2023-12-01 19:16:51 +0100

    Update 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,7 +2,7 @@
 
 def magic_calculation(a, b):
     """Match bytecode provided by Holberton School."""
-    add, sub = __import__('magic_calculation_102', fromlist=('add', 'sub'))
+    from magic_calculation_102 import add, sub
 
     if a < b:
         c = add(a, b)
