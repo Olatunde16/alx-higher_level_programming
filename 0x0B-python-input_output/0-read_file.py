83a25668678ff963798d07706dbf9d38aed2d29a
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-01-09 13:12:34 +0100

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""A function that reads text file"""
+"""This module defines a text file-reading function"""
 
 
 def read_file(filename=""):
-    """Prints the content of the UTF-8 text file to stdout"""
+    """Prints the contents of a UTF8 text file"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
