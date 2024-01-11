51f89342a865e09c33407b4e6b23d993ccba7c3d
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-01-11 17:05:53 +0100

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# base.py
+
 """Defines a base model class."""
 import json
 import csv
@@ -7,12 +7,12 @@ import turtle
 
 
 class Base:
-    """Represent the base model.
+    """Base model.
 
-    Represents the "base" for all other classes in project 0x0C*.
+    This Represents the "base" for all other classes in project 0x0C*.
 
-    Attributes:
-        __nb_objects (int): The number of instantiated Bases.
+    Private Class Attributes:
+        __nb_object (int): Number of instantiated Bases.
     """
 
     __nb_objects = 0
