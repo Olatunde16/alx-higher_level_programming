17147083b9bad74f37ddb2c076b2cdecaf9a5325
Olatunde16 <olatundeolutunmibi@gmail.com>
2024-01-15 08:15:08 -0800

    Ongoing

---
+++ 0x0C-python-almost_a_circle/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.32.0"]]],"checksum":"6a7bc3ac7112a2edd3117f01577145943616ec04e9615dcd2cb30048a6917b3d","vagrant_version":"2.4.0"}
