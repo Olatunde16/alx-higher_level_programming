42fc8faa3e66d93c3d6ebd9fb7b8ea6ac17693a4
Olutunmibi Iseoluwa <olatundeolutunmibi@gmail.com>
2024-02-13 12:57:24 -0800

    SQL updated

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
 -- Prints the full description of the table first_table from the database hbtn_0c_0 in my MySQL server.
-SELECT COLUMN_NAME, COLUMN_TYPE
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA - '%DATABASE%'
-AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE 'first_table';
