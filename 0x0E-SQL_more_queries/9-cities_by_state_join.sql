7fee89744c44309a7f2435236fc21989278f08c8
Olutunmibi Iseoluwa <olatundeolutunmibi@gmail.com>
2024-02-14 10:36:21 -0800

    Ongoing

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Lists all cities contained in the database hbtn_0d_usa.
 SELECT cities.id, cities.name, states.name AS state_name
-FROM cities, states
-WHERE cities.state_id = states.id
+FROM cities
+LEFT JOIN states ON states.id = cities.state_id
 ORDER BY cities.id ASC;
