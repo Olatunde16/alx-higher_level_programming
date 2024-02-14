0e0a5fa55487673353ef7e41e1d62154a14d9bc4
Olutunmibi Iseoluwa <olatundeolutunmibi@gmail.com>
2024-02-14 11:11:49 -0800

    Advanced SQL Query 0

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,11 @@
+-- Uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter.
+SELECT tv_genres.name
+FROM tv_genres
+LEFT JOIN (
+	    SELECT tv_show_genres.genre_id
+	    FROM tv_show_genres
+	    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+	    WHERE tv_shows.title = 'Dexter'
+) AS linked_genres ON tv_genres.id = linked_genres.genre_id
+WHERE linked_genres.genre_id IS NULL
+ORDER BY tv_genres.name ASC;
