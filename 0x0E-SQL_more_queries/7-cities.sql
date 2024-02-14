794022ec884ace686e48268b670413e6140f9ca9
Olutunmibi Iseoluwa <olatundeolutunmibi@gmail.com>
2024-02-14 10:26:39 -0800

    Ongoing

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,16 +4,10 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 USE hbtn_0d_usa;
 
-CREATE TABLE IF NOT EXISTS states (
-	id INT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id)
-);
-
 CREATE TABLE IF NOT EXISTS cities (
-	id INT NOT NULL AUTO_INCREMENT,
-	name VARCHAR(256) NOT NULL,
-	state_id INT NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (state_id) REFERENCES states(id)
-);
+	    id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	    state_id INT NOT NULL,
+	    name VARCHAR(256) NOT NULL,
+	    PRIMARY KEY(id),
+	    FOREIGN KEY(state_id) REFERENCES states(id)
+	);
