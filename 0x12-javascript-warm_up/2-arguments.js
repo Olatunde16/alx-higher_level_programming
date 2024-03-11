f4df99a9558a6eb9dbd693da968d5bd64d785485
Olutunmibi Iseoluwa <olatundeolutunmibi@gmail.com>
2024-03-11 09:32:05 -0700

    Ongoing

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+	console.log('No Argument');
+} else if (args.length === 1) {
+	console.log('Argument found');
+} else {
+	console.log('Argument found');
+}
