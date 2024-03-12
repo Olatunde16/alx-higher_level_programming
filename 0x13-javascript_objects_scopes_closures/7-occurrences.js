5ce5dbffa675ad30f7559609d9d3bea684940138
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-03-12 17:03:46 +0100

    Update 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,7 @@
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
   for (let i = 0; i < list.length; i++) {
-    if (list[i] === count) {
+    if (searchElement === list[i]) {
       count++;
     }
   }
