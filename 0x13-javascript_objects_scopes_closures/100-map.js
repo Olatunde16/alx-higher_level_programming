56b6573d8876a79c40927b8c4455c34c88e58903
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-03-12 17:20:42 +0100

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const { list } = require('./100-data');
-
-const newList = list.map((value, index) => value * index);
-
-console.log("Initial list:", list);
-console.log("New list:", newList);
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));
