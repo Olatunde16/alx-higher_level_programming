e1feb06a1c2da2ba24fa8bb2cea279e891d3bd84
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-03-12 16:55:38 +0100

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,12 +2,16 @@
 const ParentSquare = require('./5-square');
 
 class Square extends ParentSquare {
-  charPrint(c) {
-    if (!c) {
+  charPrint (c) {
+    if (c === undefined) {
       c = 'X';
     }
-    for (let i = 0; i < this.width; i++) {
-      console.log(c.repeat(this.width));
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
     }
   }
 }
