6739f94f758fb8739080622437fb399a627b3043
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-03-12 07:02:00 +0100

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-  console.log('Missing number of occurrences');
+const x = parseInt(process.argv[2]);
+
+if (!isNaN(x)) {
+    for (let i = 0; i < x; i++) {
+        console.log("C is fun");
+    }
 } else {
-  const x = Number(process.argv[2]);
-  let i = 0;
-  while (i < x) {
-    console.log('C is fun');
-    i++;
-  }
+    console.log("Missing number of occurrences");
 }
