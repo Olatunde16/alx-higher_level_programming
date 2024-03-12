4e94e475cf631a8315e240408457213ea0a62021
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-03-12 16:39:57 +0100

    Update 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -8,15 +8,16 @@ class Rectangle {
   }
 
   print () {
-    if (!this.width || !this.height) {
-      return;
-    }
     for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
     }
   }
 
-    rotate () {
+  rotate () {
     const aux = this.width;
     this.width = this.height;
     this.height = aux;
