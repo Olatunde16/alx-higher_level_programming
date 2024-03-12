ca365741d3c253a04898220a0acb8d091f6bdb5f
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-03-12 17:23:05 +0100

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,12 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
+function customMap(arr, callback) {
+    const mappedArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        mappedArr.push(callback(arr[i], i, arr));
+    }
+    return mappedArr;
+}
+
 console.log(list);
-console.log(list.map((item, index) => item * index));
+console.log(customMap(list, (item, index) => item * index));
