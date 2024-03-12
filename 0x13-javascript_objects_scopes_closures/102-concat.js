957ad17e52b8595b885ba3b81f883112237abad4
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-03-12 21:29:36 +0100

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,30 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const fArg = fs.readFileSync(process.argv[2]).toString();
-const sArg = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], fArg + sArg);
+const sourceFile1 = process.argv[2];
+const sourceFile2 = process.argv[3];
+const destinationFile = process.argv[4];
+
+fs.readFile(sourceFile1, 'utf8', (err, data1) => {
+    if (err) {
+        console.error(`Error reading file ${sourceFile1}: ${err}`);
+        return;
+    }
+
+    fs.readFile(sourceFile2, 'utf8', (err, data2) => {
+        if (err) {
+            console.error(`Error reading file ${sourceFile2}: ${err}`);
+            return;
+        }
+
+        const concatenatedData = data1 + data2;
+
+        fs.writeFile(destinationFile, concatenatedData, 'utf8', (err) => {
+            if (err) {
+                console.error(`Error writing to file ${destinationFile}: ${err}`);
+                return;
+            }
+            console.log(`Files ${sourceFile1} and ${sourceFile2} concatenated to ${destinationFile}`);
+        });
+    });
+});
