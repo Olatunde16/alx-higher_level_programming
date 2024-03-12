3deb80bc77ebc90740d03cb698c3e1e8de24df36
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-03-12 07:10:35 +0100

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-  console.log('Missing size');
+const size = parseInt(process.argv[2]);
+
+if (!isNaN(size)) {
+    for (let i = 0; i < size; i++) {
+        let line = '';
+        for (let j = 0; j < size; j++) {
+            line += 'X';
+        }
+        console.log(line);
+    }
 } else {
-  const x = Number(process.argv[2]);
-  let i = 0;
-  while (i < x) {
-    console.log('X'.repeat(x));
-    i++;
-  }
+    console.log("Missing size");
 }
