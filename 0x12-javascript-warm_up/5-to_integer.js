b4a1c0accbf987c5d9c34ea693b6daf0036954c4
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-03-12 04:18:18 +0100

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,9 +3,4 @@ if (!isNaN(parseInt(process.argv[2]))) {
 	console.log('My number:', parseInt(process.argv[2]));
 } else {
 	console.log('Not a number');
-}#!/usr/bin/node
-if (!isNaN(parseInt(process.argv[2]))) {
-	console.log('My number:', parseInt(process.argv[2]));
-} else {
-	console.log('Not a number');
 }
