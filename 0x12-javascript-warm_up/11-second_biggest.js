3897e20fab731b0b77da7355a293eb94efe11c8d
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-03-12 09:10:12 +0100

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) {
-    console.log(0);
+  console.log(0);
 } else {
-    const args = process.argv.slice(2).map(Number);
-    const sortedArgs = args.sort((a, b) => b - a);
-    console.log(sortedArgs[1]);
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
