421c6bf21c04f3bc2e97503ffdcf1e64c10eb908
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-03-12 07:16:46 +0100

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,13 @@
 #!/usr/bin/node
-function add (a, b) {
-  return a + b;
+function add(a, b) {
+    return a + b;
 }
 
-console.log(add(Number(process.argv[2]), Number(process.argv[3])));
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+
+if (!isNaN(arg1) && !isNaN(arg2)) {
+    console.log(add(arg1, arg2));
+} else {
+    console.log("Missing or invalid arguments");
+}
