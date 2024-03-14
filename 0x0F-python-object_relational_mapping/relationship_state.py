4f4488944cf058e585dbaf2462d740ab9a49dfb0
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-03-14 09:49:09 +0100

    Update relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -18,6 +18,7 @@ class State(Base):
         __tablename__ (str): The table name of the class
         id (int): The State id of the class
         name (str): The State name of the class
+        cities (:obj:`City`): The Cities belongs to State
     """
 
     __tablename__ = 'states'
