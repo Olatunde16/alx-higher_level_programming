c8557980a75a71f49eedfce76250f2915b68312f
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-03-28 13:52:18 +0100

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""
-A script that takes your GitHub credentials (username 
-and password) and uses the GitHub API to display your id
-The first argument will be your username
-The second argument will be your password (in your case, 
-a personal access token as password)
+"""script that takes your GitHub credentials (username and password) and
+uses the GitHub API to display your id.
+The first argument will be your username.
+The second argument will be your password (in your case, a personal
+access token as password).
 """
 import sys
 import requests
 from requests.auth import HTTPBasicAuth
 
+
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    json_response = requests.get("https://api.github.com/user", auth=auth)
-    print(json_response.json().get("id"))
+    url = "https://api.github.com/user"
+    username = sys.argv[1]
+    password = sys.argv[2]
+    response = requests.get(url, auth=HTTPBasicAuth(username, password))
+    json_obj = response.json()
+    print(json_obj.get("id"))
